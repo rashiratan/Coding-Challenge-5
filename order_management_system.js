ed6d2f3cc1768d3e8d9279c52a4d411ac3d3900d
Rashi Ratan <rashiiratan@gmail.com>
2024-09-21 16:34:05 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -28,3 +28,27 @@ let inventory = [
 
 let orders=[]; 
 //creating array
+
+//Task 3: Create a Function to Place an Order
+
+function placeOrder(customerName, orderedItems) {
+    orderedItems.forEach((item) =>{ //usign forEach method 
+        let inventoryItem = inventory.find((product) => product.name === item.name); //using find to match product with order
+        if (inventoryItem){
+            if (item.quantity > inventoryItem.quantity){
+            console.log(`Sorry, insufficient inventory to place your order for ${item.name}. Please try again.`);
+        } //logging error due to insufficient stock
+        else if (item.quantity === inventory.quantity) {
+            inventoryItem.quantity -= item.quantity;
+            console.log( `Please not that ${item.name} is now out of stock`); //updating quantity and informing user that product is out of stock
+        }
+        else {
+            inventoryItem.quantity -= item.quantity; //reducing quantity of product in inventory
+        }}    
+        else {
+        console.log(`Sorry, we are unable to find ${item.name}`); //logging error because product not found
+    }}
+    )
+    orders.push({customerName: customerName, items: orderedItems, status: "Pending"}); //adding the order to orders array
+        return `Your order has been placed`;//informing user 
+}
